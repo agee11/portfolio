a89f5f9e32d16b250c401e156c369f045d71f1f9
agee1 <orangemadcow@yahoo.com>
2020-08-19 17:16:07 -0700

    Updates

---
+++ precache-manifest.77171f5969c04507042a034246967f06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63a5baf33159ffe4a688f0084078808b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5865204be663bfee35ec",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5e1bfafa34a08c5749de",
+    "url": "/portfolio/static/css/main.43c878da.chunk.css"
+  },
+  {
+    "revision": "5865204be663bfee35ec",
+    "url": "/portfolio/static/js/2.9277ed3d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.9277ed3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e1bfafa34a08c5749de",
+    "url": "/portfolio/static/js/main.988e0bf6.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
