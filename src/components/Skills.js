69d6387c309aaa5f2eea348250a6436065f1d5f7
agee1 <orangemadcow@yahoo.com>
2020-08-25 21:35:05 -0700

    Change style of My Skills section and change color scheme

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,41 +1,48 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faJsSquare, faHtml5, faGithub, faCss3, faReact, faBootstrap, faNodeJs, faJira} from '@fortawesome/free-brands-svg-icons'
+import {faDesktop, faDatabase, faTools} from "@fortawesome/free-solid-svg-icons";
 
 class Skills extends React.Component{
 
 
   render(){
     return <div id="resume" className="section-container">
-    <h1 className="section-title skills-title">My Skills</h1>
+    <h1 className="section-title">My Skills</h1>
     <div className="skills-content skills-container">
       <div>
-        <h3>FrontEnd Skills</h3>
+        <FontAwesomeIcon icon={faDesktop} size="3x"/><h3>FrontEnd Skills</h3>
+        <hr/>
         <ul className="skills-list">
-          <li><FontAwesomeIcon icon={faJsSquare} size="3x"/> <p>JavaScript</p></li>
-          <li><FontAwesomeIcon icon={faCss3} size="3x"/> <p>CSS</p></li>
-          <li><FontAwesomeIcon icon={faHtml5} size="3x"/> <p>HTML</p></li>
-          <li><FontAwesomeIcon icon={faReact} size="3x"/> <p>React</p></li>
-          <li><FontAwesomeIcon icon={faBootstrap} size="3x"/> <p>Bootstrap</p></li>
-          <li><p>jQuery</p></li>
+          <li>JavaScript</li>
+          <li>CSS</li>
+          <li>HTML</li>
+          <li>React</li>
+          <li>Bootstrap</li>
+          <li>jQuery</li>
         </ul>
       </div>
       <div>
-        <h3>BackEnd Skills</h3>
+        <FontAwesomeIcon icon={faDatabase} size="3x"/><h3>BackEnd Skills</h3>
+        <hr/>
         <ul className="skills-list">
-          <li><FontAwesomeIcon icon={faNodeJs} size="3x"/> <p>Node JS</p></li>
-          <li><p> ExpressJS</p></li>
-          <li><p> SQL</p></li>
-          <li><p> Firebase</p></li>
-          <li><p> MongoDB</p></li>
-          <li><p> Mongoose</p></li>
+          <li>Node JS</li>
+          <li>ExpressJS</li>
+          <li>SQL</li>
+          <li>Firebase</li>
+          <li>MongoDB</li>
+          <li>Mongoose</li>
         </ul>
       </div>
       <div>
-        <h3>Other Skills</h3>
+        <FontAwesomeIcon icon={faTools} size="3x"/><h3>Other Skills</h3>
+        <hr/>
         <ul className="skills-list">
-          <li><FontAwesomeIcon icon={faGithub} size="3x"/> <p>Github</p></li>
-          <li><FontAwesomeIcon icon={faJira} size="3x"/> <p>Jira</p></li>
+          <li>Github</li>
+          <li>Jira</li>
+          <li>NPM</li>
+          <li>Perforce</li>
+          <li>TargetProcess</li>
         </ul>
       </div>
     </div>
