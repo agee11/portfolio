35fa0bbd60eac56bdf8af3b09fbbbdaf953f0509
agee1 <orangemadcow@yahoo.com>
2020-08-28 18:45:23 -0700

    Updates

---
+++ precache-manifest.a98f2d3008082e1291481b0248c5ed96.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34204742b6d79ee9a3d94c017287e2e9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "741d2e9ff221fade7b3f",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b913297d9e52f29fc647",
+    "url": "/portfolio/static/css/main.90201c40.chunk.css"
+  },
+  {
+    "revision": "741d2e9ff221fade7b3f",
+    "url": "/portfolio/static/js/2.71a17630.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.71a17630.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b913297d9e52f29fc647",
+    "url": "/portfolio/static/js/main.986f6a62.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "7f9d0c185eda02ae44b899dbc4bd6098",
+    "url": "/portfolio/static/media/japan.7f9d0c18.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3ee72af5d9d556b174020c1805c0be16.js"
+  "/portfolio/precache-manifest.a98f2d3008082e1291481b0248c5ed96.js"
 );
 
 self.addEventListener('message', (event) => {
