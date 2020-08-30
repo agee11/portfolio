b386523a9b271c3f6698111580fd490c02be0620
agee1 <orangemadcow@yahoo.com>
2020-08-29 18:37:15 -0700

    Add color highlight to name

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ class About extends React.Component{
   render(){
     return <div id="home" className="section-container">
       <div className="about-text-container">
-      <h1>Andrew Gee</h1>
+      <h1>Hello, I'm <span className="name-highlight">Andrew Gee</span></h1>
       <p className="about-text">A software developer based in San Francisco.  I graduated from UC Santa Cruz with a degree in Computer Science: Computer Game Design.  I continued my education through a mixture of online bootcamps and self-study.</p>
       </div>
     </div>
